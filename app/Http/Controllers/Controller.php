280b62919cb7d5ce1a7cb2a7a8d4080cbb541080
Purvesh <ind.purvesh@gmail.com>
2016-08-30 10:51:04 +1200

    working on theme support

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,28 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Session;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use Illuminate\Support\Facades\View;
+class Controller extends BaseController {
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests,
+        AuthorizesResources,
+        DispatchesJobs,
+        ValidatesRequests;
+
     public $websiteId;
     public $defaultWebsiteId;
     public $isDefaultWebsite;
-
     public $theme;
 
     public function __construct() {
 
-        $this->theme = "default";
-        $this->websiteId        = Session::get('website_id');
+        $path = realpath(base_path('themes/default'));
+        View::addLocation($path);
+        $this->websiteId = Session::get('website_id');
         $this->defaultWebsiteId = Session::get('default_website_id');
         $this->isDefaultWebsite = Session::get('is_default_website');
-
     }
+
 }
