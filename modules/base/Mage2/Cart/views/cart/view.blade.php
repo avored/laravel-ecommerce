7503b54841a9f48a3f545b5cd16a75fb6d8a9bde
Purvesh <ind.purvesh@gmail.com>
2016-09-17 09:18:17 +1200

    fixing: just html/css classes

--- modules/base/Mage2/Cart/views/cart/view.blade.php
+++ modules/base/Mage2/Cart/views/cart/view.blade.php
@@ -1,114 +1,113 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12 col-md-12">
-            <h2>Cart Page</h2>
-            @if(count($cartProducts) <= 0)
-            <p>Sorry No Product Found</p>
-            @else
-            <div class="cart-list">
-                <div class="col-sm-8 col-md-8">Product</div>
-                <div class="col-sm-1 col-md-1" style="text-align: center">Quantity</div>
-                <div class="col-md-1 text-center">Price</div>
-                <div class="col-md-1 text-center">Total</div>
-                <div class="col-md-1"> </div>
-                <?php $total = 0; ?>
-                @foreach($cartProducts as $product)
-                <div class="clearfix">
-                    <div class="col-md-12">
+<div class="row">
+    <div class="col s12">
+        <h2>Cart Page</h2>
+        @if(count($cartProducts) <= 0)
+        <p>Sorry No Product Found</p>
+        @else
+        <div class="cart-list">
+            <div class="col s8">Product</div>
+            <div class="col s1" style="text-align: center">Quantity</div>
+            <div class="col s1 ">Price</div>
+            <div class="col s1">Total</div>
+            <div class="col s1"> </div>
+            <?php $total = 0; ?>
+            @foreach($cartProducts as $product)
+            <div class="clearfix">
+                <div class="col s12">
 
-                        {!! Form::open(['method' => 'put', 'route' => 'cart.update']) !!}
+                    {!! Form::open(['method' => 'put', 'route' => 'cart.update']) !!}
 
-                        <div class="col-sm-8 col-md-8">
-                            <div class="media">
+                    <div class="col s8">
+                        <div class="media">
 
-                                @if(isset($product['model']->getProductImages($first = true)->value))
-                                <img alt="{{ $product['model']->title }}"
-                                     class="img-responsive"
-                                     style="height: 72px;"
-                                     src="{{ asset('/uploads/catalog/images/'. $product['model']->getProductImages($first= true)->value) }}" />
-                                @else 
-                                <img alt="{{ $product['model']->title }}"
-                                     class="img-responsive"
-                                     style="height: 72px;"
-                                     src="{{ asset('/img/default-product.jpg') }}" />
-                                @endif
+                            @if(isset($product['model']->getProductImages($first = true)->value))
+                            <img alt="{{ $product['model']->title }}"
+                                 class="img-responsive"
+                                 style="height: 72px;"
+                                 src="{{ asset('/uploads/catalog/images/'. $product['model']->getProductImages($first= true)->value) }}" />
+                            @else 
+                            <img alt="{{ $product['model']->title }}"
+                                 class="img-responsive"
+                                 style="height: 72px;"
+                                 src="{{ asset('/img/default-product.jpg') }}" />
+                            @endif
 
-                                <div class="media-body">
-                                    <h4 class="media-heading">
-                                        <a href="{{ route('product.view', $product['model']->slug)}}">
-                                            {{ $product['model']->title }}
-                                        </a>
-                                    </h4>
+                            <div class="media-body">
+                                <h4 class="media-heading">
+                                    <a href="{{ route('product.view', $product['model']->slug)}}">
+                                        {{ $product['model']->title }}
+                                    </a>
+                                </h4>
 
-                                    <span>Status: </span><span class="text-success"><strong>In Stock</strong></span>
-                                </div>
+                                <span>Status: </span><span class="text-success"><strong>In Stock</strong></span>
                             </div>
                         </div>
-                        <div class="col-sm-1 col-md-1" style="text-align: center">
-                            <input type="text" class="form-control" name="qty"
-                                    value="{{ $product['qty']}}">
-                            <input type="hidden" name="id" value="{{$product['model']->id}}" />
-                        </div>
-                        <?php $total += ($product['price'] * $product['qty'] ) ?>
-                        <div class="col-sm-1 col-md-1 text-center"><strong>${{ $product['price']}}</strong></div>
-                        <div class="col-sm-1 col-md-1 text-center"><strong>${{ ($product['price'] * $product['qty'] )}}</strong></div>
-                        <div class="col-sm-1 col-md-1">
-                            <a href="#" onclick="jQuery(this).parents('form:first').submit()" class="btn btn-primary">
-                                <span class="glyphicon glyphicon-edit"></span> Update
-                            </a>
-                            <a href="{{ route('cart.destroy', $product['model']->id)}}" class="btn btn-danger">
-                                <span class="glyphicon glyphicon-remove"></span> Remove
-                            </a>
-                        </div>
-                        {!! Form::close() !!}
                     </div>
-                    @endforeach
-                    <div class="clearfix"></div>
-                    <div class="col-md-12">
-                        <div class="col-md-8 col-sm-8"> &nbsp;  </div>
-                        <div class="col-md-1">&nbsp;   </div>
-                        <div class="col-md-1"> &nbsp;   </div>
-                        <div class="col-md-1"><h5>Subtotal</h5></div>
-                        <div class="col-md-1 text-right"><h5><strong>${{ $total }}</strong></h5></div>
+                    <div class="col s1">
+                        <input type="text" class="form-control" name="qty"
+                               value="{{ $product['qty']}}">
+                        <input type="hidden" name="id" value="{{$product['model']->id}}" />
                     </div>
-
-                    <div class="col-md-12">
-                        <div class="col-md-8 col-sm-8">   </div>
-                        <div class="col-md-1">   </div>
-                        <div class="col-md-1">   </div>
-                        <div class="col-md-1"><h5>Estimated shipping</h5></div>
-                        <div class="col-md-1 text-right"><h5><strong>$0.00</strong></h5></div>
+                    <?php $total += ($product['price'] * $product['qty'] ) ?>
+                    <div class="col-sm-1 col s1 text-center"><strong>${{ $product['price']}}</strong></div>
+                    <div class="col-sm-1 col s1 text-center"><strong>${{ ($product['price'] * $product['qty'] )}}</strong></div>
+                    <div class="col-sm-1 col s1">
+                        <a href="#" onclick="jQuery(this).parents('form:first').submit()" >
+                            <span class="glyphicon glyphicon-edit"></span> Update
+                        </a>
+                        <a href="{{ route('cart.destroy', $product['model']->id)}}" >
+                            <span class="glyphicon glyphicon-remove"></span> Remove
+                        </a>
                     </div>
-                    <div class="col-md-12">
-                        <div class="col-md-8 col-sm-8">   </div>
-                        <div class="col-md-1">   </div>
-                        <div class="col-md-1">   </div>
-                        <div class="col-md-1"><h3>Total</h3></div>
-                        <div class="col-md-1 text-right"><h3><strong>${{ $total }}</strong></h3></div>
-                    </div>
-                    <div class="col-md-12">
-                        <div class="col-md-6 col-sm-8">   </div>
-                        <div class="col-md-1">   </div>
-                        <div class="col-md-1">   </div>
-                        <div class="col-md-2"> 
-                            <a href="{{ route('home') }}" class="btn btn-default">
-                                <span class="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
-                            </a>
-                        </div>
-                        <div class="col-md-2 text-right"> 
-                            <a href="{{ route('checkout.index') }}" class="btn btn-success">
-                                Checkout <span class="glyphicon glyphicon-play"></span>
-                            </a>
-                        </div>
-                    </div>
-
+                    {!! Form::close() !!}
+                </div>
+                @endforeach
+                <div class="clearfix"></div>
+                <div class="col s12">
+                    <div class="col s8"> &nbsp;  </div>
+                    <div class="col s1">&nbsp;   </div>
+                    <div class="col s1"> &nbsp;   </div>
+                    <div class="col s1"><h6>Subtotal</h6></div>
+                    <div class="col s1 text-right"><h6><strong>${{ $total }}</strong></h6></div>
                 </div>
 
-                @endif
+                <div class="col s12">
+                    <div class="col s8">   </div>
+                    <div class="col s1">   </div>
+                    <div class="col s1">   </div>
+                    <div class="col s1"><h6>Estimated shipping</h6></div>
+                    <div class="col s1 text-right"><h6><strong>$0.00</strong></h6></div>
+                </div>
+                <div class="col s12">
+                    <div class="col s8">   </div>
+                    <div class="col s1">   </div>
+                    <div class="col s1">   </div>
+                    <div class="col s1"><h6>Total</h6></div>
+                    <div class="col s1 text-right"><h6><strong>${{ $total }}</strong></h6></div>
+                </div>
+                <div class="col s12">
+                    <div class="col s6">   </div>
+                    <div class="col s1">   </div>
+                    <div class="col s1">   </div>
+                    <div class="col s2"> 
+                        <a href="{{ route('home') }}" class="btn btn-default">
+                            <span class="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
+                        </a>
+                    </div>
+                    <div class="col s2 text-right"> 
+                        <a href="{{ route('checkout.index') }}" class="btn btn-success">
+                            Checkout <span class="glyphicon glyphicon-play"></span>
+                        </a>
+                    </div>
+                </div>
+
             </div>
+
+            @endif
         </div>
     </div>
+</div>
     @endsection
--- modules/base/Mage2/Catalog/views/category/view.blade.php
+++ modules/base/Mage2/Catalog/views/category/view.blade.php
@@ -1,46 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            @foreach($category->products as $product)
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        
+<div class="row">
+    <div class="col-md-12">
+        @if(count($category->products) <= 0)
+        <p>Sorry No Product Found</p>
+        @else
+        @foreach($category->products as $product)
+        <div class="col-md-4">
+            <div class="thumbnail">
+
+                <a href="{{ route('product.view', $product->slug)}}" title="{{ $product->title }}">
+                    @if(isset($product->getProductImages($first = true)->value))
+                    <img alt="{{ $product->title }}"
+                         class="img-responsive"
+                         src="{{ asset('/uploads/catalog/images/'. $product->getProductImages($first= true)->value) }}" />
+                    @else 
+                    <img alt="{{ $product->title }}"
+                         class="img-responsive"
+                         src="{{ asset('/img/default-product.jpg') }}" />
+                    @endif
+                </a>
+                <div class="caption">
+                    <h3>
                         <a href="{{ route('product.view', $product->slug)}}" title="{{ $product->title }}">
-                            @if(isset($product->getProductImages($first = true)->value))
-                                <img alt="{{ $product->title }}"
-                                     class="img-responsive"
-                                     src="{{ asset('/uploads/catalog/images/'. $product->getProductImages($first= true)->value) }}" />
-                            @else 
-                                <img alt="{{ $product->title }}"
-                                     class="img-responsive"
-                                     src="{{ asset('/img/default-product.jpg') }}" />
-                            @endif
+                            {{ $product->title }}
                         </a>
-                        <div class="caption">
-                            <h3>
-                                <a href="{{ route('product.view', $product->slug)}}" title="{{ $product->title }}">
-                                {{ $product->title }}
-                                </a>
-                            </h3>
-                            <p>
-                                {{ $product->price }}
-                            </p>
-                            <p>
-                                <a class="btn btn-primary" href="{{ route('cart.add-to-cart', $product->id) }}">Add to Cart</a>
-                                @if(isset(Auth::user()->id) && Auth::user()->isInWishlist($product->id))
-                                    <a class="btn btn-danger" href="{{ route('wishlist.remove', $product->id) }}">Remove from Wishlist</a>
-                                @else
-                                    <a class="btn btn-warning" href="{{ route('wishlist.add', $product->id) }}">Add to Wishlist</a>
-                                @endif
-                            </p>
-                        </div>
-                    </div>
+                    </h3>
+                    <p>
+                        {{ $product->price }}
+                    </p>
+                    <p>
+                        <a class="btn btn-primary" href="{{ route('cart.add-to-cart', $product->id) }}">Add to Cart</a>
+                        @if(isset(Auth::user()->id) && Auth::user()->isInWishlist($product->id))
+                        <a class="btn btn-danger" href="{{ route('wishlist.remove', $product->id) }}">Remove from Wishlist</a>
+                        @else
+                        <a class="btn btn-warning" href="{{ route('wishlist.add', $product->id) }}">Add to Wishlist</a>
+                        @endif
+                    </p>
                 </div>
-            @endforeach
+            </div>
         </div>
+        @endforeach
+        @endif
     </div>
 </div>
 @endsection
