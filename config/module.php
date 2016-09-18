f7302e730811673c39f143540b4acbfc7856fdf4
Purvesh <ind.purvesh@gmail.com>
2016-09-19 09:02:36 +1200

    moved modules array into config

---
+++ config/module.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "Mage2\\Common\\"       => base_path('/modules/base/Mage2/Common'),
+    "Mage2\\Cart\\"         => base_path('/modules/base/Mage2/Cart'),
+    "Mage2\\Install\\"      => base_path('/modules/base/Mage2/Install'),
+    "Mage2\\Catalog\\"      => base_path('/modules/base/Mage2/Catalog'),
+    "Mage2\\Order\\"        => base_path('/modules/base/Mage2/Order'),
+    "Mage2\\Attribute\\"    => base_path('/modules/base/Mage2/Attribute'),
+    "Mage2\\Address\\"      => base_path('/modules/base/Mage2/Address'),
+    "Mage2\\User\\"         => base_path('/modules/base/Mage2/User'),
+    "Mage2\\TaxClass\\"     => base_path('/modules/base/Mage2/TaxClass'),
+    "Mage2\\Wishlist\\"     => base_path('/modules/base/Mage2/Wishlist'),
+    "Mage2\\Checkout\\"     => base_path('/modules/base/Mage2/Checkout'),
+];
