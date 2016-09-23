7748a624d06b4bbca3f48a883f29756f2804847e
Purvesh <ind.purvesh@gmail.com>
2016-09-23 19:32:09 +1200

    mage2:migration command for module migration

--- modules/base/Mage2/Common/Console/Commands/Mage2Migrate.php
+++ modules/base/Mage2/Common/Console/Commands/Mage2Migrate.php
@@ -3,16 +3,21 @@
 namespace Mage2\Common\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Console\ConfirmableTrait;
 use Illuminate\Database\Migrations\Migrator;
+use Symfony\Component\Console\Input\InputOption;
 
 class Mage2Migrate extends Command {
+
+    use ConfirmableTrait;
 
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'mage2:migrate';
+    protected $signature = 'mage2:migrate {--database=} {--force} {--path=} {--pretend} {--seed} {--step}';
 
     /**
      * The console command description.
@@ -38,12 +43,10 @@ class Mage2Migrate extends Command {
      */
     public function handle() {
         $this->info("Building Command Artisan");
-        return;
 
         if (!$this->confirmToProceed()) {
             return;
         }
-
         $this->prepareDatabase();
 
         // Next, we will check to see if a path option has been defined. If it has
@@ -82,6 +85,31 @@ class Mage2Migrate extends Command {
 
             $this->call('migrate:install', $options);
         }
+    }
+
+    public function getMigrationPaths() {
+        $mage2Modules = config('module');
+        $migrationPath = [];
+        foreach ($mage2Modules as $namespace => $path) {
+            $migrationPath [] = $path . DIRECTORY_SEPARATOR . "migration";
+        }
+        return $migrationPath;
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions() {
+        return [
+            ['database', null, InputOption::VALUE_OPTIONAL, 'The database connection to use.'],
+            ['force', null, InputOption::VALUE_NONE, 'Force the operation to run when in production.'],
+            ['path', null, InputOption::VALUE_OPTIONAL, 'The path of migrations files to be executed.'],
+            ['pretend', null, InputOption::VALUE_NONE, 'Dump the SQL queries that would be run.'],
+            ['seed', null, InputOption::VALUE_NONE, 'Indicates if the seed task should be re-run.'],
+            ['step', null, InputOption::VALUE_NONE, 'Force the migrations to be run so they can be rolled back individually.'],
+        ];
     }
 
 }
