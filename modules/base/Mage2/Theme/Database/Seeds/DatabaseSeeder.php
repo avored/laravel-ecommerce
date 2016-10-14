3f95f61d3ca6906fad9433909955d72820fdb0b8
Purvesh <ind.purvesh@gmail.com>
2016-10-14 14:57:17 +1300

    theme module.

---
+++ modules/base/Mage2/Theme/Database/Seeds/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+namespace Mage2\Address\Database\Seeds;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //$this->call(Mage2EcommerceData::class);
+    }
+}
---
+++ modules/base/Mage2/Theme/Models/Theme.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mage2\Theme\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Theme extends Model
+{
+    protected $fillable = [];
+
+}
