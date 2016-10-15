a8d97a5eb10a56b5f655d0b2b98bc826af5bb3b5
Purvesh <ind.purvesh@gmail.com>
2016-10-16 11:49:08 +1300

    category page Paging Added

--- modules/base/Mage2/Catalog/Controllers/CategoryViewController.php
+++ modules/base/Mage2/Catalog/Controllers/CategoryViewController.php
@@ -4,6 +4,7 @@ namespace Mage2\Catalog\Controllers;
 
 use Mage2\Catalog\Models\Category;
 use Mage2\Framework\Http\Controllers\Controller;
+use Mage2\Common\Models\Configuration;
 
 class CategoryViewController extends Controller
 {
@@ -15,11 +16,12 @@ class CategoryViewController extends Controller
     }
 
     public function view($slug) {
-
+        $productsOnCategoryPage = Configuration::getConfiguration('mage2_catalog_no_of_product_category_page');
         $category = Category::where('slug','=',$slug)->get()->first();
-
+        $products = $category->products()->paginate($productsOnCategoryPage);
         return view('category.view')
                     ->with('category', $category)
+                    ->with('products', $products)
             ;
 
     }
--- modules/base/Mage2/Catalog/views/category/view.blade.php
+++ modules/base/Mage2/Catalog/views/category/view.blade.php
@@ -6,11 +6,18 @@
         @if(count($category->products) <= 0)
         <p>Sorry No Product Found</p>
         @else
-        @foreach($category->products as $product)
+        <?php $i =0; ?>
+        @foreach($products as $product)
         <div class="col s4">
             @include('product.view.product-card',['product' => $product])
         </div>
+        <?php  $i++; ?>
+        @if($i %3 == 0 && $i > 0)
+        <div class="clearfix"></div>
+        @endif
         @endforeach
+        <div class="clearfix"></div>
+        {!!  $products->links() !!}
         @endif
     </div>
 </div>
