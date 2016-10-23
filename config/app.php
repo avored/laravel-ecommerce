9e5755550943840b54af395428ef182eba33f1ca
Purvesh <ind.purvesh@gmail.com>
2016-10-23 17:42:04 +1300

    styleci fix

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
       | any other location as required by the application or its packages.
      */
 
-    'name' => 'Mage2 Ecommerce',
+    'name'  => 'Mage2 Ecommerce',
     /*
       |--------------------------------------------------------------------------
       | Application Environment
@@ -23,7 +23,7 @@ return [
       | services your application utilizes. Set this in your ".env" file.
       |
      */
-    'env' => env('APP_ENV', 'production'),
+    'env'   => env('APP_ENV', 'production'),
     /*
       |--------------------------------------------------------------------------
       | Application Debug Mode
@@ -78,7 +78,7 @@ return [
       | the language folders that are provided through your application.
       |
      */
-    'fallback_locale' => 'en',
+    'fallback_locale'   => 'en',
     /*
       |--------------------------------------------------------------------------
       | Encryption Key
@@ -89,8 +89,8 @@ return [
       | will not be safe. Please do this before deploying an application!
       |
      */
-    'key' => env('APP_KEY'),
-    'cipher' => 'AES-256-CBC',
+    'key'   => env('APP_KEY'),
+    'cipher'    => 'AES-256-CBC',
     /*
       |--------------------------------------------------------------------------
       | Logging Configuration
@@ -103,7 +103,7 @@ return [
       | Available Settings: "single", "daily", "syslog", "errorlog"
       |
      */
-    'log' => env('APP_LOG', 'single'),
+    'log'   => env('APP_LOG', 'single'),
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
     /*
       |--------------------------------------------------------------------------
@@ -174,40 +174,40 @@ return [
      */
     'aliases' => [
 
-        'App' => Illuminate\Support\Facades\App::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'Cookie' => Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => Illuminate\Support\Facades\Crypt::class,
-        'DB' => Illuminate\Support\Facades\DB::class,
-        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
-        'Event' => Illuminate\Support\Facades\Event::class,
-        'File' => Illuminate\Support\Facades\File::class,
-        'Gate' => Illuminate\Support\Facades\Gate::class,
-        'Hash' => Illuminate\Support\Facades\Hash::class,
-        'Lang' => Illuminate\Support\Facades\Lang::class,
-        'Log' => Illuminate\Support\Facades\Log::class,
-        'Mail' => Illuminate\Support\Facades\Mail::class,
-        'Notification' => Illuminate\Support\Facades\Notification::class,
-        'Password' => Illuminate\Support\Facades\Password::class,
-        'Queue' => Illuminate\Support\Facades\Queue::class,
-        'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
-        'Request' => Illuminate\Support\Facades\Request::class,
-        'Response' => Illuminate\Support\Facades\Response::class,
-        'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Session' => Illuminate\Support\Facades\Session::class,
-        'Storage' => Illuminate\Support\Facades\Storage::class,
-        'URL' => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
-        'Theme' => Mage2\Framework\Theme\Facade\Theme::class
+        'App'           => Illuminate\Support\Facades\App::class,
+        'Artisan'       => Illuminate\Support\Facades\Artisan::class,
+        'Auth'          => Illuminate\Support\Facades\Auth::class,
+        'Blade'         => Illuminate\Support\Facades\Blade::class,
+        'Cache'         => Illuminate\Support\Facades\Cache::class,
+        'Config'        => Illuminate\Support\Facades\Config::class,
+        'Cookie'        => Illuminate\Support\Facades\Cookie::class,
+        'Crypt'         => Illuminate\Support\Facades\Crypt::class,
+        'DB'            => Illuminate\Support\Facades\DB::class,
+        'Eloquent'      => Illuminate\Database\Eloquent\Model::class,
+        'Event'         => Illuminate\Support\Facades\Event::class,
+        'File'          => Illuminate\Support\Facades\File::class,
+        'Gate'          => Illuminate\Support\Facades\Gate::class,
+        'Hash'          => Illuminate\Support\Facades\Hash::class,
+        'Lang'          => Illuminate\Support\Facades\Lang::class,
+        'Log'           => Illuminate\Support\Facades\Log::class,
+        'Mail'          => Illuminate\Support\Facades\Mail::class,
+        'Notification'  => Illuminate\Support\Facades\Notification::class,
+        'Password'      => Illuminate\Support\Facades\Password::class,
+        'Queue'         => Illuminate\Support\Facades\Queue::class,
+        'Redirect'      => Illuminate\Support\Facades\Redirect::class,
+        'Redis'         => Illuminate\Support\Facades\Redis::class,
+        'Request'       => Illuminate\Support\Facades\Request::class,
+        'Response'      => Illuminate\Support\Facades\Response::class,
+        'Route'         => Illuminate\Support\Facades\Route::class,
+        'Schema'        => Illuminate\Support\Facades\Schema::class,
+        'Session'       => Illuminate\Support\Facades\Session::class,
+        'Storage'       => Illuminate\Support\Facades\Storage::class,
+        'URL'           => Illuminate\Support\Facades\URL::class,
+        'Validator'     => Illuminate\Support\Facades\Validator::class,
+        'View'          => Illuminate\Support\Facades\View::class,
+        'Form'          => Collective\Html\FormFacade::class,
+        'Html'          => Collective\Html\HtmlFacade::class,
+        'PDF'           => Barryvdh\DomPDF\Facade::class,
+        'Theme'         => Mage2\Framework\Theme\Facade\Theme::class
     ],
 ];
