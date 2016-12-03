014f29b6893008db25546f9837b1d26d62fa7d0f
Purvesh <ind.purvesh@gmail.com>
2016-12-03 16:01:01 +1300

    mage2 feature module added route file

---
+++ modules/community/Mage2/Feature/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+  |--------------------------------------------------------------------------
+  | Mage2 Cart Module Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an mage2 cart modules.
+  | It's a breeze. Simply tell mage2 cart module the URI it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
+Route::group(['middleware' => ['web', 'website'], 'namespace' => "Mage2\Feature\Controllers"], function () {
+    
+
+});
