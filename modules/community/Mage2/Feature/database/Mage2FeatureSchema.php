61517d6826147fc742b9aa64cfa494cc1a3a980c
Purvesh <ind.purvesh@gmail.com>
2017-01-10 19:17:17 +1300

    working on module install with schema file

--- modules/community/Mage2/Feature/database/Mage2FeatureSchema.php
+++ modules/community/Mage2/Feature/database/Mage2FeatureSchema.php
@@ -3,8 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Mage2\Catalog\Models\ProductAttribute;
 
+use Mage2\Catalog\Models\ProductAttribute;
+use Mage2\Catalog\Models\AttributeDropdownOption;
+use Mage2\Catalog\Models\ProductAttributeGroup;
 class Mage2FeatureSchema extends Migration
 {
     /**
@@ -14,6 +16,30 @@ class Mage2FeatureSchema extends Migration
      */
     public function install()
     {
+        
+        $productAttributeGroup = ProductAttributeGroup::where('title', '=','Basic')->get()->first();
+
+        $featureAttribute = ProductAttribute::create([
+            'title' => 'Is Featured',
+            'product_attribute_group_id' => $productAttributeGroup->id,
+            'identifier' => 'is_featured',
+            'type' => 'VARCHAR',
+            'field_type' => 'SELECT',
+            'sort_order' => 10,
+            'validation' => 'required',
+        ]);
+
+        AttributeDropdownOption::create([
+            'product_attribute_id' => $featureAttribute->id,
+            'value' => '0',
+            'label' => 'No',
+        ]);
+
+        AttributeDropdownOption::create([
+            'product_attribute_id' => $featureAttribute->id,
+            'value' => '1',
+            'label' => 'Yes',
+        ]);
 
         dd('install');
     }
@@ -25,6 +51,10 @@ class Mage2FeatureSchema extends Migration
      */
     public function uninstall()
     {
+       
+        $attribute  = ProductAttribute::where('identifier','=','is_featured')->get()->first();
+        
+        $attribute->delete();
             
     }
 }
