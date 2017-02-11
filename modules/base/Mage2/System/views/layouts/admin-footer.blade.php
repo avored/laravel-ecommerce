f639c7c197d36f344dd4122afcb89ec8af468038
Purvesh <ind.purvesh@gmail.com>
2017-02-11 15:13:39 +1300

    added footer file in admin layouts

---
+++ modules/base/Mage2/System/views/layouts/admin-footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <hr/>
+    <div class="container">
+        <div class="col-md-12">
+
+        <span class="pull-right">
+            <a href="http://mage2.website" title="Mage2 Company" target="_blank">Mage2</a> &copy; {{ date('Y') }}
+        </span>
+        </div>
+    </div>
+</footer>
