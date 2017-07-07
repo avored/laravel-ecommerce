09e119c6004477aecff14b823d3a12539b7fa634
ind.purvesh <ind.purvesh@gmail.com>
2017-07-07 12:57:46 +1200

    mage2 category module

--- modules/base/Mage2/Category/views/admin/catalog/category/_fields.blade.php
+++
@@ -1,4 +0,0 @@
-{!! Form::text('name','Category Name',['autofocus'=>true,'class' => 'form-control']) !!}
-{!! Form::text('slug','Category Slug') !!}
-{!! Form::select('parent_id','Parent Category',$categoryOptions) !!}
-
