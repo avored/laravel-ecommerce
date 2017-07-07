c89aaa67564b6a07d2ce009c6b6fae1126da9822
indpurvesh <ind.purvesh@gmail.com>
2017-07-08 11:18:03 +1200

    remove all base module

--- modules/base/Mage2/Sale/views/admin/gift-coupon/_fields.blade.php
+++
@@ -1,7 +0,0 @@
-{!! Form::text('title','Title',['autofocus'=>true,'class' => 'form-control']) !!}
-{!! Form::text('code','Code') !!}
-{!! Form::text('discount','Discount') !!}
-{!! Form::text('start_date','Start Date') !!}
-{!! Form::text('end_date','End Date') !!}
-{!! Form::select('status','Status',['ENABLED' => 'Enabled','DISABLED' => 'Disabled']) !!}
-
--- modules/base/Mage2/Sale/views/admin/order-status/_fields.blade.php
+++
@@ -1,3 +0,0 @@
-{!! Form::text('title','Title',['autofocus'=>true,'class' => 'form-control']) !!}
-{!! Form::text('sort_order','Sort Order',['class' => 'form-control']) !!}
-
