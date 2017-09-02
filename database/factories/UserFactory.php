6014f3419b885db4b1c544f8ffb4dd9b72f8b5dd
indpurvesh <ind.purvesh@gmail.com>
2017-09-02 17:06:51 +1200

    updated composer.json

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Mage2\User\Models\User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
