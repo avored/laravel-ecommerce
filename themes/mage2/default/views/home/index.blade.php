2ba41db4a5958a9793dd9ea8d33e3ba016345c2e
indpurvesh <ind.purvesh@gmail.com>
2017-09-05 09:25:15 +1200

    Home page display from configuration

--- themes/mage2/default/views/home/index.blade.php
+++ themes/mage2/default/views/home/index.blade.php
@@ -3,9 +3,14 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-auto" style="min-height: 450px">
+            <div class="col-12" style="min-height: 450px">
+
+                @if($pageModel !== null)
+                    {!! $pageModel->content !!}
+                @else
                 <div class="h1">Mage2 E commerce</div>
                 <h6>Home Page</h6>
+                @endif
             </div>
         </div>
     </div>
