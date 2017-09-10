0302838090a4e10b8994659730727688707e2576
indpurvesh <ind.purvesh@gmail.com>
2017-09-10 13:51:49 +1200

    remove theme files

--- themes/mage2/material/views/home/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="row">
-            <h1>Material Theme Home Page</h1>
-        </div>
-    </div>
-@endsection
