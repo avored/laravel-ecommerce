d651e18fb9dd0e56488b459337e6f388d4807ef7
Purvesh <ind.purvesh@gmail.com>
2017-10-16 14:19:03 +1300

    added few Unit Test

---
+++ tests/Feature/Admin/CategoryTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Mage2\Ecommerce\Models\Database\Category;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic Category Index Route.
+     *
+     * @return void
+     */
+    public function testCategoryIndex()
+    {
+        $response = $this->actingAs($this->adminUser,'admin')->get('/admin/category');
+        $response->assertSee('Category List');
+    }
+
+    /**
+     * Admin Category Create Route.
+     *
+     * @return void
+     */
+    public function testCategoryCreate()
+    {
+        $response = $this->actingAs($this->adminUser,'admin')->get('/admin/category/create');
+        $response->assertSee('Create Category');
+    }
+
+
+    /**
+     * Admin Category Create Route.
+     *
+     * @return void
+     */
+    public function testCategoryPost()
+    {
+        $response = $this->actingAs($this->adminUser,'admin')
+                            ->post('/admin/category', ['name' => 'Test Category Name',
+                                                        'slug' => 'test-category-name']);
+
+        $response->assertRedirect('/admin/category');
+        $this->assertDatabaseHas('categories',['slug' => 'test-category-name']);
+
+    }
+
+    /**
+     * Admin Category Edit Route.
+     *
+     * @return void
+     */
+    public function testCategoryEdit()
+    {
+
+        $category = Category::create(['name' => 'Test Category', 'slug' => 'test-category']);
+        $response = $this->actingAs($this->adminUser,'admin')
+                                            ->put("/admin/category/{$category->id}/edit");
+
+        $response->assertSee('Test Category')
+                ->assertSee('test-category');
+
+    }
+
+    /**
+     * Admin Category Update Route.
+     *
+     * @return void
+     */
+    public function testCategoryUpdate()
+    {
+
+        $category = Category::create(['name' => 'Test Category', 'slug' => 'test-category']);
+        $response = $this->actingAs($this->adminUser,'admin')
+                                ->put("/admin/category/{$category->id}", ['name' => 'Test Category Update',
+                                                                                'slug' => 'test-category-update']);
+
+        $response->assertRedirect('/admin/category');
+        $this->assertDatabaseHas('categories',['slug' => 'test-category-update']);
+
+    }
+
+
+
+    /**
+     * Admin Category Destroy Route.
+     *
+     * @return void
+     */
+    public function testCategoryDestroy()
+    {
+
+        $category = Category::create(['name' => 'Test Category', 'slug' => 'test-category']);
+
+        $response = $this->actingAs($this->adminUser,'admin')
+                                    ->delete("/admin/category/{$category->id}");
+
+        $response->assertRedirect('/admin/category');
+        $this->assertDatabaseMissing('categories',['slug' => 'test-category-update']);
+
+    }
+}
---
+++ tests/Feature/Admin/ProductTest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Mage2\Ecommerce\Models\Database\Category;
+use Mage2\Ecommerce\Models\Database\Product;
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ProductTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic Product Index Route.
+     *
+     * @return void
+     */
+    public function testProductIndex()
+    {
+        $response = $this->actingAs($this->adminUser,'admin')->get('/admin/product');
+        $response->assertSee('Product List');
+    }
+
+    /**
+     * Admin Product Create Route.
+     *
+     * @return void
+     */
+    public function testProductCreate()
+    {
+        $response = $this->actingAs($this->adminUser,'admin')->get('/admin/product/create');
+        $response->assertSee('Create Product');
+    }
+
+
+    /**
+     * Admin Product Create Route.
+     *
+     * @return void
+     */
+    public function testProductPost()
+    {
+        $response = $this->actingAs($this->adminUser,'admin')
+                            ->post('/admin/product', ['name' => 'Test Product Name',
+                                                        'type' => 'BASIC']);
+
+
+        $response->assertStatus(302);
+        //$response->assertSee('Test Product Name');
+        //$this->assertDatabaseHas('products',['name' => 'Test Product Name']);
+
+    }
+
+    /**
+     * Admin Product Edit Route.
+     *
+     * @return void
+     */
+    public function testProductEdit()
+    {
+
+        $product = Product::create(['name' => 'Test Product', 'type' => 'BASIC']);
+        $response = $this->actingAs($this->adminUser,'admin')
+                                            ->put("/admin/product/{$product->id}/edit");
+
+        $response->assertSee('Test Product')
+                ->assertStatus(200);
+
+    }
+
+    /**
+     * Admin Product Update Route.
+     *
+     * @return void
+     */
+    public function testProductUpdate()
+    {
+
+        $category = Category::create(['name' => 'Test Category', 'slug' => 'test-category']);
+        $product = Product::create(['name' => 'Test Product', 'type' => 'BASIC']);
+
+        $response = $this->actingAs($this->adminUser,'admin')
+                                ->put("/admin/product/{$product->id}", ['name' => 'Test Product Update',
+                                                                    'sku' => 'test-product-update',
+                                                                    'category_id' => [$category->id],
+                                                                    'price' => 10.00,
+                                                                    'description' => 'test description',
+                                                                    'qty' => 10,
+                                                                    'status' => 1,
+                                                                    'in_stock' => 1,
+                                                                    'track_stock' => 1,
+                                                                    'is_taxable' => 1
+                                                                    ]);
+
+        $response->assertRedirect('/admin/product')
+                                ->assertStatus(302);;
+        $this->assertDatabaseHas('products',['sku' => 'test-product-update','name' => 'Test Product Update']);
+        $this->assertDatabaseHas('product_prices',['product_id' => $product->id,'price' => 10.00]);
+        $this->assertDatabaseHas('category_product',['product_id' => $product->id,'category_id' => $category->id]);
+
+    }
+
+
+
+    /**
+     * Admin Product Destroy Route.
+     *
+     * @return void
+     */
+    public function testProductDestroy()
+    {
+
+
+        $product = Product::create(['name' => 'Test Product', 'type' => 'BASIC']);
+
+        $response = $this->actingAs($this->adminUser,'admin')
+                                    ->delete("/admin/product/{$product->id}");
+
+        $response->assertRedirect('/admin/product')
+                    ->assertStatus(302);
+        $this->assertDatabaseMissing('products',['slug' => $product->slug]);
+
+    }
+
+
+    /**
+     * Admin Product Destroy Route.
+     *
+     * @return void
+     */
+    public function testUploadFile()
+    {
+        
+        $this->assertTrue(true);
+
+    }
+
+
+}
