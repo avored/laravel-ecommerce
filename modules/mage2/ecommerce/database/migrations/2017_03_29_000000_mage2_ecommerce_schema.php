8ba7af1383c4a33e1fd62af0f3ce82e8c5d9ab11
Purvesh <ind.purvesh@gmail.com>
2017-11-15 16:29:29 +1300

    fixed bug: Database Table Creation Order

--- modules/mage2/ecommerce/database/migrations/2017_03_29_000000_mage2_ecommerce_schema.php
+++ modules/mage2/ecommerce/database/migrations/2017_03_29_000000_mage2_ecommerce_schema.php
@@ -264,20 +264,6 @@ class Mage2EcommerceSchema extends Migration
             $table->foreign('product_id')->references('id')->on('products');
         });
 
-        Schema::create('order_product_variations', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('order_id')->unsigned();
-            $table->integer('product_id')->unsigned();
-            $table->integer('attribute_id')->unsigned();
-            $table->integer('attribute_dropdown_option_id')->unsigned()->nullable()->default(null);
-            $table->timestamps();
-
-            $table->foreign('order_id')->references('id')->on('orders');
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('attribute_id')->references('id')->on('attributes');
-            $table->foreign('attribute_dropdown_option_id')->references('id')->on('attribute_dropdown_options');
-        });
-
         //order_product_variations
 
         Schema::create('gift_coupons', function (Blueprint $table) {
@@ -468,6 +454,22 @@ class Mage2EcommerceSchema extends Migration
                 ->references('id')->on('products')->onDelete('cascade');
         });
 
+        Schema::create('order_product_variations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->integer('attribute_id')->unsigned();
+            $table->integer('attribute_dropdown_option_id')->unsigned()->nullable()->default(null);
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('attribute_id')->references('id')->on('attributes');
+            $table->foreign('attribute_dropdown_option_id')->references('id')->on('attribute_dropdown_options');
+        });
+
+
+        
         Schema::create('product_combinations', function (Blueprint $table) {
             $table->increments('id');
 
