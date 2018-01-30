3b72d8f834bca6102556384ef964396a95aa4ca7
Purvesh <ind.purvesh@gmail.com>
2018-01-31 10:24:04 +1300

    Using Laravel Mix for Asset compilation

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,28 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('themes/mage2/default/assets/js/app.js', 'public/vendor/mage2-default/js')
+   .sass('themes/mage2/default/assets/sass/app.scss', 'public/vendor/mage2-default/css');
+
+
+
+mix.scripts(['modules/mage2/ecommerce/resources/assets/js/jquery-3.2.1.min.js',
+        'modules/mage2/ecommerce/resources/assets/js/popper.min.js',
+        'modules/mage2/ecommerce/resources/assets/js/bootstrap.min.js',
+        'modules/mage2/ecommerce/resources/assets/js/fontawesome-all.min.js',
+        'modules/mage2/ecommerce/resources/assets/js/Chart.min.js',
+        'modules/mage2/ecommerce/resources/assets/js/select2.min.js',
+        'modules/mage2/ecommerce/resources/assets/js/flatpickr.js'
+        ]
+            , 'public/vendor/mage2-admin/js/app.js');
+
+
+    mix.sass('modules/mage2/ecommerce/resources/assets/sass/app.scss', 'public/vendor/mage2-admin/css/sass.css')
+    .styles(['public/vendor/mage2-admin/css/bootstrap.min.css',
+            'public/vendor/mage2-admin/css/select2.min.css',
+            'public/vendor/mage2-admin/css/flatpickr.min.css',
+            'public/vendor/mage2-admin/css/styles.css',
+            'public/vendor/mage2-admin/css/sass.css'
+            ]
+            ,'public/vendor/mage2-admin/css/app.css'
+        );
