8ff9d7f07dc68d4b9a731d538509e63f405c03e7
Purvesh <ind.purvesh@gmail.com>
2018-04-11 14:31:06 +1200

    Category Filter for Product Attributes

--- themes/avored/default/views/category/options.blade.php
+++ themes/avored/default/views/category/options.blade.php
@@ -12,16 +12,15 @@
                     </li>
                 @endforeach
             </ul>
+
         @endif
-        <?php
-        $attributes = $category->getFilters();
-        ?>
-        @foreach($attributes as $att)
+        @foreach($category->categoryFilter as $filter)
 
 
-            @if(array_get($att,'type') === "ATTRIBUTE")
+
+            @if(array_get($filter,'type') === "ATTRIBUTE")
                 @php
-                    $attribute = $att->model;
+                    $attribute = $filter->model;
                 @endphp
 
 
@@ -29,31 +28,38 @@
                 <ul class="list-group">
 
                     @foreach($attribute->attributeDropdownOptions as $option)
+
+                        @php
+
+                            $attributeParams = isset($params['attribute']) ? $params['attribute'] : [];
+                            $queryParams = $params;
+
+                        @endphp
                         <li class="list-group-item">
+
                             <input id="variation-{{ $option->id }}"
                                    type="checkbox"
 
                                    class="category-variation-checkbox"
-                                   @if(in_array($attribute->identifier, array_keys($params)) && in_array($option->id, array_values($params)))
+
+                                   @if(in_array($attribute->identifier, array_keys($attributeParams)) &&
+                                        in_array($option->id, array_values($attributeParams)))
                                    {{ "checked" }}
-                                   <?php
-                                   $queryParams = $params;
-                                   unset($queryParams[$attribute->identifier])
-                                   ?>
+
                                    data-checked-url="{{ route('category.view',
-                                                                ['slug' => $category->slug,$attribute->identifier => $option->id] + $queryParams) }}"
+                                                                ['slug' => $category->slug,"attribute[" .$attribute->identifier . "]" => $option->id] + $queryParams) }}"
                                    data-unchecked-url="{{ route('category.view',
                                                                 ['slug' => $category->slug] + $queryParams) }}"
 
                                    @else
 
                                    data-checked-url="{{ route('category.view',
-                                                                    ['slug' => $category->slug,$attribute->identifier => $option->id] + $params) }}"
+                                                                    ['slug' => $category->slug,"attribute[" .$attribute->identifier . "]" => $option->id] + $params) }}"
                                    data-unchecked-url="{{ route('category.view',
                                                                     ['slug' => $category->slug] + $params) }}"
 
                                    @endif
-                                   name="{{ $attribute->identifier }}" value="{{ $option->id }}">
+                                   name="attribute[{{ $attribute->identifier }}]" value="{{ $option->id }}">
                             <label for="variation-{{ $option->id }}">
                                 {{ $option->display_text }}
                             </label>
@@ -64,55 +70,56 @@
 
             @endif
 
-                @if(array_get($att,'type') === "PROPERTY")
-                    @php
-                        $attribute = array_get($att, 'model');
-                    @endphp
-                    <?php
-                    $params = array_get($params,'property') ?? [];
-                    ?>
+            @if(array_get($filter,'type') === "PROPERTY")
+                @php
+                    $attribute = $filter->model;
+                @endphp
+                <?php
+                //$params = array_get($params, 'property') ?? [];
+                ?>
 
-                    <h4>{{ $attribute->name }}</h4>
-                    <ul class="list-group">
+                <h4>{{ $attribute->name }}</h4>
+                <ul class="list-group">
 
-                        @foreach($attribute->propertyDropdownOptions as $option)
-                            <li class="list-group-item">
-                                <input id="variation-{{ $option->id }}"
-                                       type="checkbox"
+                    @foreach($attribute->propertyDropdownOptions as $option)
+                        <li class="list-group-item">
+                            <input id="variation-{{ $option->id }}"
+                                   type="checkbox"
 
-                                       class="category-variation-checkbox"
+                                   class="category-variation-checkbox"
 
-                                       @if(in_array($attribute->identifier, array_keys($params)) &&
-                                            in_array($option->id, array_values($params)))
-                                       {{ "checked" }}
-                                       <?php
-                                       $queryParams = $params;
+                                   @if(in_array($attribute->identifier, array_keys($params)) &&
+                                        in_array($option->id, array_values($params)))
+                                   {{ "checked" }}
+                                   <?php
+                                   $queryParams = $params;
 
-                                       unset($queryParams[$attribute->identifier]);
-                                       ?>
-                                       data-checked-url="{{ route('category.view',
+                                   unset($queryParams[$attribute->identifier]);
+                                   ?>
+                                   data-checked-url="{{ route('category.view',
                                                                 ['slug' => $category->slug,"property[" .$attribute->identifier . "]" => $option->id] + $queryParams) }}"
-                                       data-unchecked-url="{{ route('category.view',
+                                   data-unchecked-url="{{ route('category.view',
                                                                 ['slug' => $category->slug] + $queryParams) }}"
 
-                                       @else
+                                   @else
 
-                                       data-checked-url="{{ route('category.view',
+                                   data-checked-url="{{ route('category.view',
                                                                     ['slug' => $category->slug,"property[" .$attribute->identifier . "]" => $option->id] + $params) }}"
-                                       data-unchecked-url="{{ route('category.view',
+                                   data-unchecked-url="{{ route('category.view',
                                                                     ['slug' => $category->slug] + $params) }}"
 
-                                       @endif
-                                       name="property[{{ $attribute->identifier }}]" value="{{ $option->id }}">
-                                <label for="variation-{{ $option->id }}">
-                                    {{ $option->display_text }}
-                                </label>
-                            </li>
-                        @endforeach
+                                   @endif
 
-                    </ul>
+                                   name="property[{{ $attribute->identifier }}]" value="{{ $option->id }}">
+                            <label for="variation-{{ $option->id }}">
+                                {{ $option->display_text }}
+                            </label>
+                        </li>
+                    @endforeach
 
-                @endif
+                </ul>
+
+            @endif
         @endforeach
 
     </div>
