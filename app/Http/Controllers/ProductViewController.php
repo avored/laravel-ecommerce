e0f2a1f81b2694ec18638a7d4f87ab9cca287024
Purvesh <ind.purvesh@gmail.com>
2018-06-29 12:25:46 +1200

    updated account controller

--- app/Http/Controllers/ProductViewController.php
+++ app/Http/Controllers/ProductViewController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use AvoRed\Framework\Models\Database\Product;
 use AvoRed\Framework\Models\Contracts\ProductInterface;
 use Illuminate\Http\Request;
 use AvoRed\Framework\Models\Contracts\ProductDownloadableUrlInterface;
@@ -15,28 +14,25 @@ class ProductViewController extends Controller
      */
     protected $repository;
 
-     /**
-     * Product Downloadable Url Repository
-     * @var \AvoRed\Framework\Models\Repository\ProductDownloadableUrlRepository
-     */
+    /**
+    * Product Downloadable Url Repository
+    * @var \AvoRed\Framework\Models\Repository\ProductDownloadableUrlRepository
+    */
     protected $downRep;
 
-    public function __construct(ProductInterface $repository , ProductDownloadableUrlInterface $downRep)
-    {
-        $this->repository   = $repository;
-        $this->downRep      = $downRep;
+    public function __construct(
+        ProductInterface $repository,
+        ProductDownloadableUrlInterface $downRep
+        ) {
+        $this->repository = $repository;
+        $this->downRep = $downRep;
     }
 
     public function view($slug)
     {
         $product = $this->repository->findBySlug($slug);
-        $title = (!empty($product->meta_title)) ?
-                                            $product->meta_title :
-                                            $product->name;
-
-        $description = (!empty($product->meta_description)) ?
-                                        $product->meta_description :
-                                        substr($product->description, 0, 255);
+        $title = $product->meta_title ?? $product->name;
+        $description = $product->meta_description ?? substr($product->description, 0, 255);
 
         return view('product.view')
                                 ->with('product', $product)
@@ -44,21 +40,19 @@ class ProductViewController extends Controller
                                 ->with('description', $description);
     }
 
-    public function downloadDemoProduct(Request $request) {
-
-        $downModel  = $this->downRep->findByToken($request->get('product_token'));
+    public function downloadDemoProduct(Request $request)
+    {
+        $downModel = $this->downRep->findByToken($request->get('product_token'));
 
         $path = storage_path('app/public' . DIRECTORY_SEPARATOR . $downModel->demo_path);
         return response()->download($path);
-
     }
 
-    public function downloadMainProduct(Request $request) {
-
-        $downModel  = $this->downRep->findByToken($request->get('product_token'));
+    public function downloadMainProduct(Request $request)
+    {
+        $downModel = $this->downRep->findByToken($request->get('product_token'));
 
         $path = storage_path('app/public' . DIRECTORY_SEPARATOR . $downModel->main_path);
         return response()->download($path);
-
     }
 }
