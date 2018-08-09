56316a293dc2d0466c9175035df6f77b33c0fe6d
Purvesh <ind.purvesh@gmail.com>
2018-08-09 17:57:31 +1200

    updates into storage symblink

--- modules/avored/dummy-data/database/seeds/AvoRedDataSeeder.php
+++ modules/avored/dummy-data/database/seeds/AvoRedDataSeeder.php
@@ -6,6 +6,7 @@ use AvoRed\Framework\Models\Database\Product;
 use Faker\Factory;
 use AvoRed\Framework\Models\Database\ProductImage;
 use AvoRed\Framework\Models\Database\Page;
+use AvoRed\Framework\Models\Database\Menu;
 use AvoRed\Framework\Models\Database\Configuration;
 
 class AvoRedDataSeeder extends Seeder
@@ -52,7 +53,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($livingRoomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/f/h/2/flower-pot.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/f/h/2/flower-pot.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $product = Product::create([
             'name' => 'Classic TV Stand',
@@ -71,7 +72,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($livingRoomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/d/0/c/classic-tv-stand.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/d/0/c/classic-tv-stand.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $product = Product::create([
             'name' => 'Classic Vintage Curtain',
@@ -90,7 +91,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($livingRoomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/y/f/r/textiles-2.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/y/f/r/textiles-2.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $product = Product::create([
             'name' => 'Comfirtable Couch',
@@ -109,7 +110,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($livingRoomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/1/k/0/-Single-Panel-New-Pastoral-Linen-Blending-Embroidered-Living-Room-font-b-Curtain-b-font-font.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/1/k/0/-Single-Panel-New-Pastoral-Linen-Blending-Embroidered-Living-Room-font-b-Curtain-b-font-font.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $product = Product::create([
             'type' => 'BASIC',
@@ -128,7 +129,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($livingRoomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/q/o/m/comfortable-leather-chair-published-under-the-most-comfortable-couch-group.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/q/o/m/comfortable-leather-chair-published-under-the-most-comfortable-couch-group.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Medium White Couch';
         $product = Product::create([
@@ -148,7 +149,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($livingRoomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/s/e/j/ff815ea7756de71d6c5edb5566330df6.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/s/e/j/ff815ea7756de71d6c5edb5566330df6.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Comfirtable Gray Bed';
         $comfirtableGrayBedProduct = Product::create([
@@ -168,7 +169,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($bedroomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/v/t/x/bed-bedding-comfortable-platform-with-smooth-gray-also-are-beds-and-headboard-plus-small-.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/v/t/x/bed-bedding-comfortable-platform-with-smooth-gray-also-are-beds-and-headboard-plus-small-.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Cute Teddy Bear';
         $product = Product::create([
@@ -188,7 +189,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($bedroomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/z/c/u/d5d710257f2cf7cf2576f4a43dc40430.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/z/c/u/d5d710257f2cf7cf2576f4a43dc40430.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Minimalist Ceramic Lamp';
         $product = Product::create([
@@ -208,7 +209,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($bedroomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/m/2/z/b594a5c88e527b467508aa9fa3b01228.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/m/2/z/b594a5c88e527b467508aa9fa3b01228.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Wooden Bunk Bed';
         $product = Product::create([
@@ -228,7 +229,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($bedroomCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/4/5/n/il_570xN.262261571.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/4/5/n/il_570xN.262261571.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Cooktail Mixed';
         $product = Product::create([
@@ -248,7 +249,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($kitchenCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/n/y/n/CC2600.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/n/y/n/CC2600.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Coffee Making Machine';
         $product = Product::create([
@@ -268,7 +269,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($kitchenCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/t/b/n/20121018143846738.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/t/b/n/20121018143846738.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Luxury Cooking Utensil';
         $product = Product::create([
@@ -288,7 +289,7 @@ class AvoRedDataSeeder extends Seeder
         ]);
 
         $product->categories()->sync($kitchenCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/l/i/k/coffee-maker-20.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/l/i/k/coffee-maker-20.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $title = 'Vintage Toaste';
         $product = Product::create([
@@ -307,36 +308,36 @@ class AvoRedDataSeeder extends Seeder
             'type' => 'BASIC',
         ]);
 
-        \AvoRed\Framework\Models\Database\Menu::create([
+        Menu::create([
             'name' => $kitchenCategory->name,
             'params' => $kitchenCategory->slug,
             'route' => 'category.view',
         ]);
-        \AvoRed\Framework\Models\Database\Menu::create([
+        Menu::create([
             'name' => $bedroomCategory->name,
             'params' => $bedroomCategory->slug,
             'route' => 'category.view',
         ]);
-        \AvoRed\Framework\Models\Database\Menu::create([
+        Menu::create([
             'name' => $livingRoomCategory->name,
             'params' => $livingRoomCategory->slug,
             'route' => 'category.view',
         ]);
-        \AvoRed\Framework\Models\Database\Menu::create([
+        Menu::create([
             'name' => 'My Account',
             'route' => 'my-account.home',
         ]);
-        \AvoRed\Framework\Models\Database\Menu::create([
+        Menu::create([
             'name' => 'Cart',
             'route' => 'cart.view',
         ]);
-        \AvoRed\Framework\Models\Database\Menu::create([
+        Menu::create([
             'name' => 'Checkout',
             'route' => 'checkout.index',
         ]);
 
         $product->categories()->sync($kitchenCategory->id);
-        ProductImage::create(['path' => 'storage/uploads/catalog/images/0/y/4/tsf02crsa.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
+        ProductImage::create(['path' => 'uploads/catalog/images/0/y/4/tsf02crsa.jpg', 'product_id' => $product->id, 'is_main_image' => 1]);
 
         $homePageContent = html_entity_decode('<p>&nbsp;</p><p>&nbsp;</p><p><strong>HOME PAGE FOR AvoRed E COMMERCE LARAVEL OPEN SOURCE SHOPPING CART</strong></p><p>&nbsp;</p><p><strong>Please star us on&nbsp;<a href="https://github.com/avored/laravel-ecommerce">https://github.com/avored/laravel-ecommerce</a></strong></p><p><strong>Like us on Facebook :&nbsp;<a href="https://www.facebook.com/avored/">https://www.facebook.com/avored/</a></strong></p><p><strong>Follow us on Twitter:&nbsp;<a href="https://twitter.com/avoredecommerce/">https://twitter.com/avoredecommerce/</a></strong></p>');
         $homePage = Page::create(['name' => 'Home Page',
