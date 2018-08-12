6ca2cc7fd716fd0c369d179c3619c24da98aaa28
Purvesh <ind.purvesh@gmail.com>
2018-08-13 11:11:28 +1200

    fixed issue #187
    Duplicate toolbar when editing a product

--- avored.admin.webpack.mix.js
+++ avored.admin.webpack.mix.js
@@ -15,5 +15,5 @@ mix.config.fileLoaderDirs.fonts     = 'vendor/avored-admin/fonts';
 mix.config.fileLoaderDirs.images    = 'vendor/avored-admin/images';
 
 
-//mix.js('modules/avored/ecommerce/resources/assets/js/app.js', 'public/vendor/avored-admin/js');
-//mix.sass('modules/avored/ecommerce/resources/assets/sass/app.scss',  'public/vendor/avored-admin/css');
+//mix.js('packages/framework/resources/assets/js/app.js', 'public/vendor/avored-admin/js');
+mix.sass('packages/framework/resources/assets/sass/app.scss',  'public/vendor/avored-admin/css');
