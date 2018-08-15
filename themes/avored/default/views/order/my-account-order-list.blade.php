d130791ce3cf704c52251e87bf7edfc0a8fb95fa
ghermans <glhermans@gmail.com>
2018-08-15 14:20:59 +0200

    blade fixes

--- themes/avored/default/views/order/my-account-order-list.blade.php
+++ themes/avored/default/views/order/my-account-order-list.blade.php
@@ -9,51 +9,37 @@
             @include('user.my-account.sidebar')
         </div>
         <div class="col-md-9">
-
-            <div class="main-title-wrap">
-                <span class="title">
-                    Order List
-                    <!--<small>Sub title</small> -->
-                </span>
-            </div>
-            <div class="clearfix"></div>
-            <br/>
-            @if(count($orders) <= 0)
-                <p>Sorry No Order Found</p>
-            @else
-                <table class="table table-bordered table-responsive">
-                    <thead>
-                    <th>ID</th>
-                    <th>Shipping Option</th>
-                    <th>Payment Option</th>
-                    <th>Status</th>
-                    <th>View</th>
-                    <!--th>Return Request</th-->
-                    </thead>
-                    <tbody>
-                    @foreach($orders as $order)
-                        <tr>
-                            <td>{{ $order->id }}</td>
-                            <td>{{ $order->shipping_option}}</td>
-                            <td>{{ $order->payment_option }}</td>
-                            <td>{{ $order->orderStatus->name }}</td>
-                            <td>
-                                <a href="{{ route('my-account.order.view',$order->id )}}">View</a>
-                            </td>
-                            <!--td>
-                                <a href=" route('my-account.order-return.create',$order->id )}}">Return Request</a>
-                            </td-->
-
-
-                        </tr>
-                    @endforeach
-
-                    </tbody>
-
-                </table>
-            @endif
-
-        </div>
-    </div>
+            <div class="card">
+                <div class="card-header">My orders</div>
+                <div class="card-body">
+                    @if(count($orders) <= 0)
+                        <p>We couldn't find any records.</p>
+                    @else
+                        <table class="table table-striped">
+                            <thead>
+                                <th>ID</th>
+                                <th>Shipping Option</th>
+                                <th>Payment Option</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </thead>
+                            <tbody>
+                                @foreach($orders as $order)
+                                    <tr>
+                                        <td>{{ $order->id }}</td>
+                                        <td>{{ $order->shipping_option}}</td>
+                                        <td>{{ $order->payment_option }}</td>
+                                        <td>{{ $order->orderStatus->name }}</td>
+                                        <td><a href="{{ route('my-account.order.view',$order->id )}}">View</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>            
 @endsection
 
