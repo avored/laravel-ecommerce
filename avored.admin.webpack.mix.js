d0f138aeed032aa5f6e54057cd06925c90555779
Glenn Hermans <glhermans@gmail.com>
2018-09-29 01:51:04 +0200

    Temp fix

--- avored.admin.webpack.mix.js
+++ avored.admin.webpack.mix.js
@@ -16,4 +16,4 @@ mix.config.fileLoaderDirs.images    = 'vendor/avored-admin/images';
 
 mix.js('vendor/avored/framework/resources/assets/js/app.js', 'public/vendor/avored-admin/js')
     .sass('vendor/avored/framework/resources/assets/sass/app.scss', 'public/vendor/avored-admin/css')
-    .copyDirectory('vendor/avored/framework/resources/assets/static/images','public/vendor/avored-admin/images');
+   // .copyDirectory('vendor/avored/framework/resources/assets/static/images','public/vendor/avored-admin/images');
