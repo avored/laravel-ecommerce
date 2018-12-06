d427e0c6b181ca878a65258c27f007f9a7f6a8d5
ghermans <glhermans@gmail.com>
2018-12-06 06:10:43 +0100

    First progress on adding translations #18

---
+++ themes/avored/default/lang/en/customer.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'first_name'         => 'First name',
+    'last_name'          => 'Last name',
+    'email'              => 'E-mail address',
+    'phone'              => 'Phone number',
+    'mobile'             => 'Mobile number',
+    'postalcode'         => 'Zip/Postal code',
+    'address'            => 'Address',
+    'city'               => 'City',
+    'state'              => 'State',
+    'country'            => 'Country',
+    'password'           => 'Password',
+    'confirm_password'   => 'Confirm Password',
+    'register_account'   => 'Create a new account',
+];
--- themes/avored/default/views/checkout/cards/personal.blade.php
+++ themes/avored/default/views/checkout/cards/personal.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-header">
-        Your Personal Details
+        {{ __('checkout.personal_title') }}
     </div>
     <div class="card-body">
         @php
@@ -14,15 +14,13 @@
 
         <div class="row">
             <div class="form-group  col-6">
-                <label class="control-label" for="input-user-first-name">First Name</label>
-                <input type="text" name="billing[first_name]"
-                       value="{{ $firstName }}" placeholder="First Name"
-                       id="input-user-first-name"
-                       @if($errors->has('billing.first_name'))
+                <label class="control-label" for="input-user-first-name">{{ __('customer.first_name') }} <span class="text-danger">*</span></label>
+                <input type="text" name="billing[first_name]" value="{{ $firstName }}" id="input-user-first-name"
+                      @if($errors->has('billing.first_name'))
                        class="is-invalid avored-checkout-field form-control"
-                       @else
+                      @else
                        class="form-control avored-checkout-field"
-                        @endif
+                      @endif
                 />
                 @if ($errors->has('billing.first_name'))
                     <div class="invalid-feedback">
@@ -33,14 +31,13 @@
             </div>
 
             <div class="form-group  col-6">
-                <label class="control-label" for="input-user-last-name">Last Name</label>
+                <label class="control-label" for="input-user-last-name">{{ __('customer.last_name') }} <span class="text-danger">*</span></label>
                 <input type="text" name="billing[last_name]"
-                       value="{{ $lastName }}" placeholder="Last Name"
-                       id="input-user-last-name"
+                       value="{{ $lastName }}" id="input-user-last-name"
                         @if($errors->has('billing.last_name'))
-                            class="is-invalid form-control avored-checkout-field"
+                          class="is-invalid form-control avored-checkout-field"
                         @else
-                            class="form-control avored-checkout-field"
+                          class="form-control avored-checkout-field"
                         @endif
                 />
                 @if ($errors->has('billing.last_name'))
@@ -54,12 +51,8 @@
         @if(!Auth::check())
 
             <div class="form-group">
-                <label class="control-label" for="input-user-email">E-Mail</label>
-                <input
-                        type="text"
-                        name="user[email]" placeholder="E-Mail"
-                        id="input-user-email"
-
+                <label class="control-label" for="input-user-email">{{ __('customer.email') }} <span class="text-danger">*</span></label>
+                <input type="text" name="user[email]" id="input-user-email"
                         @if($errors->has('user.email'))
                         class="is-invalid avored-checkout-field form-control"
                         @else
@@ -77,8 +70,8 @@
                 <label>
                     <input type="checkbox" name="register"
                            onclick="if (this.checked == true) jQuery('.register-form').css('display','block');
-                                                      else jQuery('.register-form').css('display','none');">
-                    &nbsp;Register Account
+                                    else jQuery('.register-form').css('display','none');">
+                    &nbsp;{{ __('checkout.register_account') }}
                 </label>
             </div>
 
@@ -87,15 +80,13 @@
                 <div class="row">
                     <div class="form-group  col-6">
                         <label class="control-label"
-                               for="input-billing-password">Password</label>
-                        <input type="password" name="user[password]" placeholder="Password"
-                               id="input-billing-password"
-
-                               @if($errors->has('user.password'))
+                               for="input-billing-password">{{ __('customer.password') }} <span class="text-danger">*</span></label>
+                        <input type="password" name="user[password]" id="input-billing-password"
+                              @if($errors->has('user.password'))
                                class="is-invalid avored-checkout-field form-control"
-                               @else
+                              @else
                                class="form-control avored-checkout-field"
-                                @endif
+                              @endif
                         />
                         @if ($errors->has('user.password'))
                             <div class="invalid-feedback">
@@ -104,16 +95,13 @@
                         @endif
                     </div>
                     <div class="form-group  col-6">
-                        <label class="control-label" for="input-billing-confirm">Password
-                            Confirm</label>
-                        <input type="password" name="user[confirm_password]"
-                               placeholder="Password Confirm"
-                               id="input-billing-confirm"
+                        <label class="control-label" for="input-billing-confirm">{{ __('customer.confirm_password') }} <span class="text-danger">*</span></label>
+                        <input type="password" name="user[confirm_password]" id="input-billing-confirm" 
                                @if($errors->has('user.confirm_password'))
-                               class="is-invalid avored-checkout-field form-control"
+                                class="is-invalid avored-checkout-field form-control"
                                @else
-                               class="form-control avored-checkout-field"
-                                @endif
+                                class="form-control avored-checkout-field"
+                               @endif
                         />
 
                         @if ($errors->has('user.confirm_password'))
@@ -124,18 +112,16 @@
                     </div>
                 </div>
             </div>
-
         @endif
 
         <div class="form-group">
-            <label class="control-label" for="input-billing-phone">Phone</label>
-            <input type="text" name="billing[phone]" value="{{ $phone }}" placeholder="Phone"
-                   id="input-billing-phone"
-                   @if($errors->has('billing.phone'))
+            <label class="control-label" for="input-billing-phone">{{ __('customer.phone') }} <span class="text-danger">*</span></label>
+            <input type="text" name="billing[phone]" value="{{ $phone }}" id="input-billing-phone"
+                  @if($errors->has('billing.phone'))
                    class="is-invalid form-control avored-checkout-field"
-                   @else
+                  @else
                    class="form-control avored-checkout-field"
-                    @endif
+                  @endif
             />
             @if ($errors->has('billing.phone'))
                 <div class="invalid-feedback">
