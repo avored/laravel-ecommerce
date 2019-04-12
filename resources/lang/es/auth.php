105e09e38460b7de7b930fc22e6b5389b14de781
Diego Garcia <diegog368@gmail.com>
2019-04-12 12:35:55 -0300

    wip: first translations in spanish language

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'authenticate'     => 'Identificarse',
+    'create'           => 'Crear una cuenta',
+    'email'            => 'Email',
+    'password'         => 'Contraseña',
+    'change_password'  => 'Modificar contraseña',
+    'current_password' => 'Contraseña actual',
+    'new_password'     => 'Nueva contraseña',
+    'confirm_password' => 'Confirmar contraseña',
+    'failed'           => 'Error de usuario o contraseña.',
+    'reset_password'   => 'Recuperar contraseña',
+    'login'            => 'Identificarse',
+    'remember'         => 'Recuérdame',
+    'throttle'         => 'Muchos intentos. Por favor reintente en :seconds segundos.',
+    'social_auth'      => 'o use su cuenta para',
+    'save'             => 'Guardar preferencias',
+    'signin'           => 'Entrar',
+    'signin_title'     => 'Entrar en su cuenta',
+    'facebook'         => 'Entrar con Facebook',
+    'google'           => 'Entrar con Google',
+    'twitter'          => 'Entrar con Twitter',
+];
---
+++ resources/lang/es/payments.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Payment methods Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+   'creditcard'        => 'Tarjeta de Crédito',
+   'klarna'            => 'Klarna',
+   'ideal'             => 'iDeal',
+   'bancontact'        => 'Bancontact',
+   'paypal'            => 'PayPal',
+   'giropay'           => 'Giropay',
+   'eps'               => 'EPS',
+   'belfius'           => 'Belfius Direct Net',
+   'ing'               => "ING Home'Pay",
+   'kbc'               => 'KBC/CBC',
+   'sofort'            => 'SOFORT Banking',
+   'giftcard'          => 'Gift card',
+   'bitcoin'           => 'Bitcoin',
+   'paysafecard'       => 'Paysafecard',
+   'sepa-direct-debit' => 'SEPA Direct Debit',
+   'pickup'            => 'Paying at pickup'
+ ];
