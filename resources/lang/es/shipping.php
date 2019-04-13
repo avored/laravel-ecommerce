1556e1d11e18fe00560b992b9f7cd12b02701623
Diego Garcia <diegog368@gmail.com>
2019-04-12 21:28:27 -0300

    translation to spanish language

---
+++ resources/lang/es/shipping.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Shipping language lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'freeshipping'    => 'Envío gratis',
+    'pickup'          => 'Retirar en sucursal',
+    'today'           => 'Entrega el mismo día',
+    'next_day'        => 'Entrega al siguiente día',
+];
---
+++ resources/lang/es/wishlist.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'my_wishlist'    => 'Favoritos',
+    'product_title'  => 'Nombre del producto',
+    'product_image'  => 'Imagen del producto',
+    'actions'        => 'Acciones',
+    'remove'         => 'Quitar',
+    'no_items'       => 'Actualmente no tienes productos en el carrito'
+];
