09e5591b2c3f19f957ceaf55c3025ea2d167c9ea
Purvesh <ind.purvesh@gmail.com>
2019-07-07 16:09:52 +1200

    category filter

---
+++ resources/components/CategoryPage.vue
@@ -0,0 +1,45 @@
+<script>
+import isNil from 'lodash/isNil'
+import querystring from  'querystring'
+import isArray  from 'lodash/isArray';
+
+export default {
+    props: ['currentUrl', 'filterProp'],
+    data() {
+        return {
+            filter: {}
+        }
+    },
+    methods: {
+        filterCheckboxChange(e, filterSlug, filterValue, type) {
+            if (e.target.checked) {
+                if (type === 'PROPERTY') {
+                    var arraySlug = 'p___' + filterSlug + '[]';
+                }
+                if (isNil(this.filter[arraySlug])) {
+                    this.filter[arraySlug] = [];
+                }
+                //console.log(querystring, 'herre');
+                this.filter[arraySlug].push(filterValue);
+                if (this.filter[arraySlug].length > 1) {
+                    const url = this.currentUrl + '?' + querystring.stringify(this.filter);
+                    location = url;
+                }
+                
+            } else {
+                console.log('un checked box')
+            }
+        }
+    },
+    mounted() {
+        for (var key  in this.filterProp) {
+            if (isArray(this.filterProp[key])) {
+                let arrayKey = key + '[]';
+                this.filter[arrayKey] = [];
+                this.filter[arrayKey] = this.filterProp[key];
+            }
+        }
+        
+    }
+}
+</script>
--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<a-row :gutter="15" >
-  <a-col :span="18" :offset="3">
-    <a-row :gutter="15" justify="center" align="center">
-      @foreach ($category->products as $product)
-          <a-col :span="8">
-            @include('category.card.product', ['product' => $product])
-          </a-col>
-      @endforeach
-    </a-row>
-  </a-col>
-</a-row>
+<category-page
+  current-url="{{ request()->url() }}"
+  :filter-prop="{{ json_encode(request()->all()) }}"
+  inline-template>
+  
+  <a-row :gutter="15" >
+    <a-col :span="6">
+      @include('category.card.filters')
+    </a-col>
+    <a-col :span="18">
+      <a-row :gutter="15" justify="center" >
+        @foreach ($categoryProducts as $product)
+            <a-col :span="8">
+              @include('category.card.product', ['product' => $product])
+            </a-col>
+        @endforeach
+      </a-row>
+    </a-col>
+  </a-row>
+</category-page>
 @endsection
