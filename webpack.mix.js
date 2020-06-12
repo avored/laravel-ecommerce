e1d0da68d28f55c0defcf66e693f3268c870fc7b
Purvesh <purvesh.patel@digistorm.com>
2020-06-12 16:43:56 +1000

    upgraded node packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -13,11 +14,11 @@ const tailwindcss = require('tailwindcss')
     
 
 
-mix.webpackConfig({
-    output: {
-        chunkFilename: mix.inProduction() ? "avored-admin/js/chunk/[name].[chunkhash].js" : "avored-admin/js/chunk/[name].js",
-    }
-})
+// mix.webpackConfig({
+//     output: {
+//         chunkFilename: mix.inProduction() ? "avored-admin/js/chunk/[name].[chunkhash].js" : "avored-admin/js/chunk/[name].js",
+//     }
+// })
 
 
 // mix.webpackConfig({
@@ -25,40 +26,46 @@ mix.webpackConfig({
 //         chunkFilename: mix.inProduction() ? "js/chunk/[name].[chunkhash].js" : "js/chunk/[name].js",
 //     }
 // })
-mix.js('resources/js/app.js', 'public/js/app.js');
+// mix.js('resources/js/app.js', 'public/js/app.js');
 
-mix.less('resources/less/app.less', 'public/css/app.css', {
-    javascriptEnabled: true,
-    modifyVars: {
-        'primary-color': '#E64448',
-        'link-color': '#C12E32',
-        'border-radius-base': '5px',
-    },
-}).options({
-    processCssUrls: false,
-    postCss: [ tailwindcss('tailwind.config.js') ],
-})
+// mix.less('resources/less/app.less', 'public/css/app.css', {
+//     javascriptEnabled: true,
+//     modifyVars: {
+//         'primary-color': '#E64448',
+//         'link-color': '#C12E32',
+//         'border-radius-base': '5px',
+//     },
+// }).options({
+//     processCssUrls: false,
+//     postCss: [ tailwindcss('tailwind.config.js') ],
+// })
 
-let url = process.env.APP_URL.replace(/(^\w+:|^)\/\//, '');
-mix.options({
-   hmrOptions: {
-       host: url,
-       port: 8080 // Can't use 443 here because address already in use
-   }
-});
+
+// let url = process.env.APP_URL.replace(/(^\w+:|^)\/\//, '')
+// mix.options({
+//    hmrOptions: {
+//        host: url,
+//        port: 8080 // Can't use 443 here because address already in use
+//    }
+// })
 
 
 /******** AVORED ADMIN JS  **********/
-mix.js('packages/framework/resources/js/app.js', 'public/avored-admin/js/app.js');
+let publicPath = './public/avored-admin'
+
+mix.setPublicPath(publicPath)
+
+mix.js('packages/framework/resources/js/app.js', 'public/avored-admin/js/app.js')
+
 /******** AVORED ADMIN CSS  **********/
-mix.less('packages/framework/resources/less/app.less', 'public/avored-admin/css/app.css', {
-    javascriptEnabled: true,
-    modifyVars: {
-        'primary-color': '#E64448',
-        'link-color': '#C12E32',
-        'border-radius-base': '5px',
-    },
-}).options({
-    processCssUrls: false,
-    postCss: [ tailwindcss('tailwind.config.js') ],
-})
+// mix.less('packages/framework/resources/less/app.less', 'public/avored-admin/css/app.css', {
+//     javascriptEnabled: true,
+//     modifyVars: {
+//         'primary-color': '#E64448',
+//         'link-color': '#C12E32',
+//         'border-radius-base': '5px',
+//     },
+// }).options({
+//     processCssUrls: false,
+//     postCss: [ tailwindcss('tailwind.config.js') ],
+// })
