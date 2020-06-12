718c30b55565536e75457a4769d321734907acb8
Purvesh <ind.purvesh@gmail.com>
2020-06-13 09:12:46 +1000

    remove banner as a default module

--- modules/avored/banner/resources/js/banner.js
+++
@@ -1,4 +0,0 @@
-AvoRed.initialize((Vue) => {
-    Vue.component('banner-table', require('../components/BannerTable.vue').default)    
-    Vue.component('banner-edit', require('../components/BannerEdit.vue').default)    
-})
