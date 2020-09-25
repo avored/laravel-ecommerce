486cb2994cef018c70f0c3f2fe5791189bccca33
AlexMalikov94 <alex.malikov94@gmail.com>
2020-09-25 08:19:00 +0500

    Update Home namespace

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Home;
 
 use AvoRed\Framework\Database\Contracts\PageModelInterface;
 use AvoRed\Framework\Database\Contracts\ProductModelInterface;
