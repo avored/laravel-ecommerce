64da962b000fe3aac12ccf3e6ac985f89e3eb052
AlexMalikov94 <alex.malikov94@gmail.com>
2020-09-29 13:50:59 +0500

    Add button save and cancel

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -58,6 +58,23 @@
                     input-type="email"
                     error-text="{{ $errors->first('email') }}"
                 ></avored-input>
+                <div class="mt-3 py-3">
+                    <button type="submit"
+                        class="px-6 py-3 font-semibold leading-7  text-white hover:text-white bg-red-600 rounded hover:bg-red-700"
+                    >   
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 inline-flex w-4" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M0 2C0 .9.9 0 2 0h14l4 4v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5 0v6h10V2H5zm6 1h3v4h-3V3z"/>
+                        </svg>
+                        <span class="ml-3">{{ __('avored.btn.save') }}</span>
+                    </button>
+                    
+                    <a href="{{ route('account.dashboard') }}"
+                        class="px-6 py-3 font-semibold inline-block text-white leading-7 hover:text-white bg-gray-500 rounded hover:bg-gray-600">
+                        <span class="leading-7">
+                            {{ __('avored.btn.cancel') }}
+                        </span>
+                    </a>
+                </div>
             </div>
           
         </form>
