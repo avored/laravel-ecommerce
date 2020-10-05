df694db1b4aee4099e9b0a17bea2cbf745a3b63b
AlexMalikov94 <alex.malikov94@gmail.com>
2020-10-05 10:05:40 +0500

    Cart is empty if statement and change name validation to OrderPlaceRequest

--- app/Http/Requests/Order/OrderPlaceRequest.php
+++ app/Http/Requests/Order/OrderPlaceRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Order;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CustomerRequest extends FormRequest
+class OrderPlaceRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
