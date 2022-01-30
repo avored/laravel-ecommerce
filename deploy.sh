5a671f00e9c2493541a10c217517e0113a4bff34
indpurvesh <ind.purvesh@gmail.com>
2022-01-30 13:17:29 +1300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/avored/laravel-ecommerce.git master:gh-pages
+cd ..
