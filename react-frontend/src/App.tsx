82badedcbe1b610180efc6ce4f7526a2d09c487d
Purvesh <ind.purvesh@gmail.com>
2022-11-09 08:38:37 +1300

    setup tailwind css and headless ui

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Listbox } from '@headlessui/react'
 
-function App() {
+
+
+const people = [
+  { id: 1, name: 'Durward Reynolds', unavailable: false },
+  { id: 2, name: 'Kenton Towne', unavailable: false },
+  { id: 3, name: 'Therese Wunsch', unavailable: false },
+  { id: 4, name: 'Benedict Kessler', unavailable: true },
+  { id: 5, name: 'Katelyn Rohan', unavailable: false },
+]
+
+
+export default function App() {
+  const [selectedPerson, setSelectedPerson] = useState(people[0])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container mx-auto">
+      <div className="flex justify-center h-screen items-center">
+        <h1 className="text-xl font-bold ">
+          AvoRed React E commerce {' '}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">
+            Switch (Toggle)
+          </span>
+        </h1>
+        <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+          <Listbox.Button>{selectedPerson.name}</Listbox.Button>
+          <Listbox.Options>
+            {people.map((person) => (
+              <Listbox.Option
+                key={person.id}
+                value={person}
+                disabled={person.unavailable}
+              >
+                {person.name}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Listbox>
 
-export default App;
+      </div>
+    </div>
+  )
+}
