62a428da75cb83fd245acb1141288c14b7ed0c69
Purvesh <ind.purvesh@gmail.com>
2022-11-15 10:05:50 +1300

    tailwind setup

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 import { Index } from './pages/Index';
 import { Product } from './pages/Product'
@@ -8,10 +8,18 @@ import { Product } from './pages/Product'
 
 function App() {
   return (
+    <>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/product">Product</Link></li>
+          </ul>
+        </nav>
         <Routes>
           <Route path='/' element={<Index />} />
           <Route path='/product' element={<Product />} />
       </Routes>
+    </>
   );
 }
 
--- react-frontend/src/pages/Index.tsx
+++ react-frontend/src/pages/Index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Index = () => {
   return (
-    <div>Index</div>
+    <h1 className="text-3xl text-blue-600 font-bold underline">
+      Hello world!
+    </h1>
   )
 }
