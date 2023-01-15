d62d75fa727b870d50c8c7b25d8116894a54904e
Purvesh <ind.purvesh@gmail.com>
2023-01-16 11:00:32 +1300

    move the graphql query/mutation into seperate file

--- react-frontend/src/pages/auth/LoginPage.tsx
+++ react-frontend/src/pages/auth/LoginPage.tsx
@@ -3,6 +3,7 @@ import { useMutation } from "urql";
 import Logo from "../../logo.svg";
 import { LockClosedIcon } from '@heroicons/react/24/solid'
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { loader } from 'graphql.macro'
 import {
   setAuthInfo,
   isAuth,
@@ -14,56 +15,9 @@ import { useNavigate } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { FormLabel } from "../../components/Form/FormLabel";
 import { FormInput } from "../../components/Form/FormInput";
-import { Link } from "react-router-dom";
 import { FormattedMessage, useIntl } from "react-intl";
 import { FormLink } from "../../components/Form/FormLink";
 import { FormButton } from "../../components/Form/FormButton";
-
-
-const CustomerLogin = `
-    mutation CustomerLogin (
-      $password: String!
-      $email: String
-    ) {
-    login (
-      email: $email
-      password: $password
-    ) {
-      first_name
-        last_name
-        email
-        image_path_url
-        id
-        created_at
-        updated_at
-        addresses {
-            id
-            type
-            customer_id
-            first_name
-            last_name
-            company_name
-            address1
-            address2
-            postcode
-            city
-            state
-            country_id
-            phone
-            created_at
-            updated_at
-        }
-        token_info {
-            token_type
-            access_token
-            expires_in
-            refresh_token
-        }
-      }
-    }
-`;
-
-
 declare global {
   interface Window { x: any; }
 }
@@ -75,8 +29,8 @@ export const LoginPage = () => {
   const navigate = useNavigate();
   const intl = useIntl()
 
-
-  const [customerLoginResult, customerLogin] = useMutation(CustomerLogin)
+  const CustomerLoginQuery = loader('../../graphql/mutation/auth/Login.graphql')
+  const [customerLoginResult, customerLogin] = useMutation(CustomerLoginQuery)
 
   const dispatch = useAppDispatch();
 
@@ -162,14 +116,8 @@ export const LoginPage = () => {
                 <div>
                     <FormButton type="submit">
                       <LockClosedIcon className="absolute w-7 h-7 opacity-40 inset-y-1 left-0 flex items-center pl-3" />
-                      Sign in
+                      <FormattedMessage id="sign_in" />
                     </FormButton>
-                  {/* <button
-                    type="submit"
-                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-red-500 focus:ring-offset-1"
-                  >
-                    
-                  </button> */}
                 </div>
               </form>
           </div>
