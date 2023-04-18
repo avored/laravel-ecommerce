016627e854b9adf3e515286f15467c5def29f8fb
Purvesh <ind.purvesh@gmail.com>
2023-04-19 10:22:16 +1200

    Basic setup of alpine and tailwind css

--- laravel-backend/resources/js/app.js
+++ laravel-backend/resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+
+
+
+Alpine.data('dropdown', () => ({
+    open: false,
+ 
+    toggle() {
+        this.open = ! this.open
+    }
+}))
+
+
+
+Alpine.start()
--- laravel-backend/vite.config.js
+++ laravel-backend/vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+import fs from 'fs';
+import { homedir } from 'os';
+import { resolve } from 'path';
+
+// Ignore the protocol on the host, ie do not put "http"
+const host = 'laravel-backend.test';
+
+const viteServerConfig = host => {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+    if (!fs.existsSync(keyPath)) {
+        return {}
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {}
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: { 
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    }
+}
+
 export default defineConfig({
+    server: viteServerConfig(host),
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
